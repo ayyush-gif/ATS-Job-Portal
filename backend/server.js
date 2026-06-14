080fbb58df33d303edc68ec4006ed9810098b415
Ayush Verma <vermayush590@gmail.com>
2026-06-14 19:58:14 +0530

    Updated production API URL

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectDB();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin:"https://ats-job-portal-ecru.vercel.app", credentials: true,
+}));
+
+
 app.use(express.json());
 app.use(
   "/uploads",
